b355c18b27600473f28f60639a5d7d2efaed452b
Chad Smith <chadsmith4@my.unt.edu>
2019-03-09 17:13:24 -0600

    basic login flow for the application with basic form validation

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -4,27 +4,78 @@
       <v-toolbar-title>Login form</v-toolbar-title>
     </v-toolbar>
     <v-card-text>
-      <v-form>
-        <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+      <v-form v-model="isFormValid">
+        <v-text-field
+          prepend-icon="person"
+          name="login"
+          label="Login"
+          type="text"
+          v-model="username"
+          :rules="userNameRules"
+        ></v-text-field>
         <v-text-field
           id="password"
           prepend-icon="lock"
           name="password"
           label="Password"
           type="password"
+          v-model="password"
+          :rules="passwordRules"
         ></v-text-field>
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary" :disabled="!isFormValid || isFormSubmitting" @click="handleLogin">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { SessionModule } from "@/store/modules/session";
+import { SnackbarModule } from "@/store/modules/snackbar";
+import SnackbarMessage from "@/core/user-interface-models/Snackbar";
+import { SnackbarType } from "@/core/user-interface-models/ISnackbar";
 
 @Component({})
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  isFormValid: boolean = false;
+  isFormSubmitting: boolean = false;
+  username: string = "";
+  password: string = "";
+
+  get userNameRules() {
+    return [(v: string) => !!v || "Username is required..."];
+  }
+
+  get passwordRules() {
+    return [
+      (v: string) => !!v || "Please enter a password...",
+      (v: string) => v.length >= 6 || "Password must be at least 8 characters"
+    ];
+  }
+
+  handleLogin() {
+    if (this.isFormValid) {
+      this.isFormSubmitting = true;
+
+      SessionModule.Login({ username: this.username, password: this.password })
+        .then(response => {
+          this.$router.replace({
+            name: "Home"
+          });
+        })
+        .catch(error => {
+          this.isFormSubmitting = false;
+          this.isFormValid = false;
+          this.password = "";
+          var snackbarMessage = new SnackbarMessage();
+          snackbarMessage.snackBarType = SnackbarType.Error;
+          snackbarMessage.message = error.message;
+          SnackbarModule.SHOW_SNACKBAR(snackbarMessage);
+        });
+    }
+  }
+}
 </script>
